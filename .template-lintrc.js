fc39e01ec90b5d3e351d9a73de2ef2cc370d1ea6
BenjaminJ <kamikillerto@gmail.com>
2020-06-05 11:07:16 +0200

    fix: update template-lint config to stop reporting error for`context-id`

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  extends: 'octane',
+  rules: {
+    'no-implicit-this': {
+      allow: ['context-id']
+    },
+    'no-curly-component-invocation': {
+      allow: ['context-id']
+    }
+  }
 };
